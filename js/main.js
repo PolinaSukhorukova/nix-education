01f7dfc3c8546acaf47a064f1de3359ca1e34cc3
PolinaSukhorukova <sukhorukovapolina03@gmail.com>
2022-04-29 00:09:28 +0300

    burger-menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-$(function() {
-	
-	
-	
-	
-});
+var iconMenu = document.querySelector('.menu__icon');
+if (iconMenu) {
+	var menuBody = document.querySelector('.menu');
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+
+	});
+}
